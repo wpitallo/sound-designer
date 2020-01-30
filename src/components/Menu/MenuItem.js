75fb4aa3f461d0715f99b88d3ac2a671b6a21bd6
waffs <wpitallo@gmail.com>
2020-01-30 10:15:10 +0200

    fix bugs

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -4,7 +4,7 @@ import { Button } from "devextreme-react";
 export default class MenuItem extends React.Component {
   render() {
     return (
-      <div style={{ "margin-top": "10px" }}>
+      <div style={{ marginTop: "10px" }}>
         <Button icon="trash" onClick={this.plusClick} />
         <Button style={{ height: "36px", width: "235px" }}>
           {this.props.item.name}
